5901a12547e7154db800a5a443c15422dba7f00d
HorvathMate04 <culubpenguin@gmail.com>
2024-09-20 23:57:38 +0200

    kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +11,35 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get("/quotes")
+  @Render('quotes')
+  getQuotes() {
+    return this.appService.getQuotes();
+  }
+
+  @Get("/randomQuote")
+  @Render('randomQuote')
+  getRandomQuote() {
+    return {result: this.appService.randomizeQuote()};
+  }
+
+  @Get("/topAuthors")
+  @Render('topAuthors')
+  getTopAuthors() {
+    return {result: this.appService.countQuotes()};
+  }
+
+  @Get('/quotes/:id')
+  @Render('randomQuote')
+  getQuoteById(@Param('id') id: string) {
+    return { result: this.appService.getQuoteById(id) };
+  }
+
+  @Get('/deleteQuote/:id')
+  @Render('delete')
+  deleteQuote(@Param('id') id: string) {
+    return { result: this.appService.deleteQuoteById(id) };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { list} from './quotes';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getQuotes(){
+    return list;
+  }
+
+  randomizeQuote(){
+    return list.quotes[Math.floor(Math.random() * list.quotes.length)];
+  }
+
+  countQuotes(){
+    let result = {};
+    for(const quote of list.quotes){
+      if(result.hasOwnProperty(quote.author)){
+        result[quote.author]++;
+      }else{
+        result[quote.author] = 1;
+      }
+    }
+    let sorted = {};
+    for(const key in result){
+      let max = result[key]
+      let maxKey = key
+      for(const comp in result){
+        if(max < result[comp]){
+          max = result[comp];
+          maxKey = comp
+        }
+      }
+      sorted[maxKey] = max;
+      delete result[maxKey];
+    }
+    return sorted
+  }
+
+  getQuoteById(id) {
+    for(const quote of list.quotes){
+      if(quote.id == id){
+        return quote;
+      }
+    }
+    return null;
+  }
+
+  deleteQuoteById(id){
+    for(let i = 0; i < list.quotes.length; i++){
+      if(list.quotes[i].id == id){
+        list.quotes.splice(i, 1)
+        return true;
+      }
+    }
+    return false;
   }
 }
